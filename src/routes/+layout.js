257c181efb70aa6aff6f3bab35ec2e00b9011436
Github1234fab <fabien.marceau@gmail.com>
2026-05-19 11:06:41 +0200

    accéleration d'ouverture

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,12 @@
+import { prerendering } from '$app/environment';
+
 export async function load({ url }) {
+	if (prerendering) {
+		return {
+			from_app: false
+		};
+	}
+
 	const fromAppParam = url.searchParams.get('from_app');
 	return {
 		from_app: fromAppParam === 'true'
